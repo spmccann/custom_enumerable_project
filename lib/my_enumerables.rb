4ebe0c4b2b9487c6a8b173ed8c98c1d4f03fbf6c
Scott <scottpmccann@gmail.com>
2022-05-01 16:02:09 -0700

    solve custom enumerables

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,129 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    x = 0
+    while x < self.length
+      yield(self[x])
+      x += 1
+    end
+    self
+  end
+
+  def my_each_with_index
+    x = 0
+    while x < self.length
+      yield(self[x], x)
+      x += 1
+    end
+    self
+  end
+
+  def my_select
+    x = 0
+    arr = []
+    while x < self.length
+      if yield(self[x])
+        arr.push(self[x])
+      end
+      x += 1
+    end
+    arr
+  end
+
+  def my_all?
+    x = 0
+    count = 0
+    arr = []
+    while x < self.length
+      if yield(self[x])
+        arr.push(self[x])
+      else
+        count += 1
+      end
+      x += 1
+    end
+    if count == 0
+      true 
+    else
+      false
+    end
+  end
+
+  def my_any?
+    x = 0
+    count = 0
+    arr = []
+    while x < self.length
+      if yield(self[x])
+        arr.push(self[x])
+        count += 1
+      end
+      x += 1
+    end
+    if count > 0
+      true 
+    else
+      false
+    end
+  end
+
+  def my_none?
+    x = 0
+    count = 0
+    arr = []
+    while x < self.length
+      if yield(self[x])
+        arr.push(self[x])
+        count += 1
+      end
+      x += 1
+    end
+    if count > 0
+      false
+    else
+      true
+    end
+  end
+
+  def my_count
+    x = 0
+    count = 0
+    while x < self.length
+      if block_given? 
+        if yield(self[x])
+          count += 1
+        end
+      else
+        count += 1
+      end
+      x += 1
+    end
+    count
+  end
+
+  def my_map
+    x = 0
+    arr = []
+    while x < self.length
+      if block_given?
+        arr.push(yield(self[x]))
+      else
+        arr.push(self[x])
+      end
+      x += 1
+    end
+    arr
+  end
+
+  def my_inject(initial_value)
+    x = 0
+    value = 0
+    while x < self.length
+      value = yield(initial_value, self[x])
+      initial_value = value
+      x += 1
+    end
+    initial_value
+  end
 end
